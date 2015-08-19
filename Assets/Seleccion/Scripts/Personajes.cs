e253504c4d4d53ad4cfd5587f1f27686d25e1d91
Sergio Erik Madrigal Herrera <sergio.madrigal_smh@hotmail.com>
2015-08-19 11:30:58 -0500

    orden
    orden de carpetas

---
+++ Assets/Seleccion/Scripts/Personajes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Personajes : MonoBehaviour {
+
+	public GameObject selectSlot;
+	public GameObject modelo;
+
+	// Use this for initialization
+	void Start () {
+	
+		modelo.SetActive(false);
+
+	}
+	
+	// Update is called once per frame
+	void OnMouse(){
+   
+		if(selectSlot){
+			modelo.SetActive(true);
+		}
+
+
+	}
+}
