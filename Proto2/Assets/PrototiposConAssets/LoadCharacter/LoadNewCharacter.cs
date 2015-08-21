10a676307d2f7081af7b5699f00c6aca17143192
Zhm17 <zhemo17@gmail.com>
2015-08-21 18:14:20 -0500

    21082015

---
+++ Proto2/Assets/PrototiposConAssets/LoadCharacter/LoadNewCharacter.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI; 
+using System.Collections;
+
+public class LoadNewCharacter : MonoBehaviour {
+
+	public Image timerImage ;
+	//public Button buttonDog, buttonBunny, buttonTurtle;
+
+	// Use this for initialization
+	void Start () {
+		timerImage = GetComponent<Image>() ;
+
+		StartCoroutine("activeButton");
+		StartCoroutine("MethodUpdate");
+	}
+	
+	// Update is called once per frame
+	IEnumerator MethodUpdate () {
+		while(true){
+
+
+
+			yield return null;
+		}
+	}
+	
+	//Timer for activate character Button
+	IEnumerator activeButton(){
+
+		timerImage.fillAmount = 0;
+
+		while(timerImage.fillAmount < 1f){
+			Debug.Log (timerImage.fillAmount);
+			timerImage.fillAmount += 0.1f;
+		}
+
+		timerImage.fillAmount = 0;
+
+		yield return null;
+	}
+
+	//disable Button when is Pressed
+	private void disableButtonPressed(Button buttonArg){
+		buttonArg.enabled = false;
+	}
+
+}
--- Proto2/Assets/PrototiposConAssets/Personaje_Disparo/BoneBullet.cs
+++ Proto2/Assets/PrototiposConAssets/Personaje_Disparo/BoneBullet.cs
@@ -2,19 +2,19 @@
 using System.Collections;
 
 public class BoneBullet : MonoBehaviour {
-
-	public GameObject point;
+	public float speed = 0.01f;
+	public Transform point;
 
 	// Use this for initialization
 	void Start () {
-		
+		transform.position = point.position;
+		StartCoroutine("MethodUpdate");
 	}
 	
 	// Update is called once per frame
 	IEnumerator MethodUpdate () {
 		while(true){
-
-
+			transform.Translate(speed,0,0);
 
 			yield return null;
 		}
