3b1498c54ef877b2e23dd34e9c27def05b2b64e4
Sergio Erik Madrigal Herrera <sergio.madrigal_smh@hotmail.com>
2015-08-21 13:54:49 -0500

    EnemigosNuevo
    nuevo

--- Assets/Enemigo/EnemyController.cs
+++ Assets/Enemigo/EnemyController.cs
@@ -3,22 +3,17 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 	public GameObject proyectil;
-	// Use this for initialization
+	public GameObject target;
 	void Start () {
 
-		InvokeRepeating ("Ball1", 1, 1);
+		InvokeRepeating ("Ball", 1, 1);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-		Destroy(proyectil);
-	}
-	
 
-	void Ball1(){
+	void Ball(){
 	GameObject go=(GameObject)Instantiate (proyectil, transform.position, Quaternion.identity);
-		go.GetComponent<Rigidbody>().velocity = new Vector3 (0,15,-100);
+		go.GetComponent<Rigidbody>().velocity = new Vector3 (0,0,-500);
+
+		Vector3 nuevo = target.transform.position - proyectil.transform.position;
 
 	}
 
